37651705a8edb3c6ffb52020dbe8cc41117cce84
luisfsosa <luisfsosa@gmail.com>
2014-05-28 16:16:44 +0000

    Se agrego el archivo Paso1.rb

---
+++ Paso1.rb
@@ -0,0 +1,15 @@
+puts  2 + 4
+
+puts "Hola Mundo"
+
+puts "Hola Mundo".reverse
+
+puts "Hola Mundo".length
+
+puts "Hola Mundo " * 5
+
+puts 123.to_s.reverse
+
+puts "123" * 3
+
+puts "123".to_i * 3
